1ea72967fe0dfc7c63f74b53d9fd6598fde0714a
gr13nka <gran-san2003@yandex.ru>
2023-10-20 18:49:08 +0300

    emacs

--- emacs/config.el
+++ emacs/config.el
@@ -1,14 +1,3 @@
-(defun system-is-linux()
-    (string-equal system-type "gnu/linux"))
-(defun system-is-windows()
-    (string-equal system-type "windows-nt"))
-
-;; Start Emacs as a server in linux/osx
-;;(when (system-is-linux)
-    ;;(require 'server)
-    ;;(unless (server-running-p)
-      ;;  (server-start))) ;; запустить Emacs как сервер, если ОС - GNU/Linux
-
 (require 'package)
 (require 'org-macs)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
@@ -22,92 +11,30 @@
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
 
-(use-package evil)
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
 
-(use-package general
-  :config
-  (general-create-definer dt/leader-keys
-    :states '(emacs)
-    :keymaps 'override
-    :prefix "SPC" ;; set leader
-    :global-prefix "M-SPC") ;; access leader in insert mode
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
 
-  (dt/leader-keys
-    "." '(find-file :wk "Find file")
-    "f c" '((lambda () (interactive) (find-file "~/.config/emacs/config.org")) :wk "Edit emacs config")
-    "f r" '(counsel-recentf :wk "Find recent files")
-    "TAB TAB" '(comment-line :wk "Comment lines"))
 
-  (dt/leader-keys
-    "b" '(:ignore t :wk "buffer")
-    "b b" '(switch-to-buffer :wk "Switch buffer")
-    "b i" '(ibuffer :wk "Ibuffer")
-    "b k" '(kill-this-buffer :wk "Kill this buffer")
-    "b n" '(next-buffer :wk "Next buffer")
-    "b p" '(previous-buffer :wk "Previous buffer")
-    "b r" '(revert-buffer :wk "Reload buffer"))
 
-  (dt/leader-keys
-    "e" '(:ignore t :wk "Eshell/Evaluate")    
-    "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
-    "e d" '(eval-defun :wk "Evaluate defun containing or after point")
-    "e e" '(eval-expression :wk "Evaluate and elisp expression")
-    "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
-    "e r" '(eval-region :wk "Evaluate elisp in region")
-    "e s" '(vterm :which-key "Eshell"))
+(setq global-auto-revert-non-file-buffers t)
 
- (dt/leader-keys
-    "h" '(:ignore t :wk "Help")
-    "h f" '(describe-function :wk "Describe function")
-    "h v" '(describe-variable :wk "Describe variable")
-    "h r r" '(reload-init-file :wk "Reload emacs config"))
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+;;(load custom-file 'noerror 'nomessage)
 
-  (dt/leader-keys
-    "t" '(:ignore t :wk "Toggle")
-    "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
-    "t t" '(visual-line-mode :wk "Toggle truncated lines")
-    "t v" '(vterm-toggle :wk "Toggle vterm"))
-  
-  (dt/leader-keys
-    "n" '(:ignore t :wk "New")
-    ;; Window splits
-    "n n" '(evil-window-vsplit :wk "Vertival new window")
-    "n s" '(vterm :wk "New terminal window")
-  )
-   
-  (dt/leader-keys
-    "w" '(:ignore t :wk "Windows")
-    ;; Window splits
-    "w c" '(evil-window-delete :wk "Close window")
-    ;; Window motions
-    "w h" '(evil-window-left :wk "Window left")
-    "w j" '(evil-window-down :wk "Window down")
-    "w k" '(evil-window-up :wk "Window up")
-    "w l" '(evil-window-right :wk "Window right")
-    "w w" '(evil-window-next :wk "Goto next window")
-    ;; Move Windows
-    "w H" '(buf-move-left :wk "Buffer move left")
-    "w J" '(buf-move-down :wk "Buffer move down")
-    "w K" '(buf-move-up :wk "Buffer move up")
-    "w L" '(buf-move-right :wk "Buffer move right"))
-)
+(setq history-length 25)
+(savehist-mode 1)
+;; Remembers your location in file
+(save-place-mode 1)
+
+(add-hook 'emacs-startup-hook #'recentf-open-files)
 
 (require 'dired)
 (setq dired-recursive-deletes 'top) ;; чтобы можно было непустые директории удалять...
-
-;; IDO plugin
-(require 'ido)
-(ido-mode                      t)
-(icomplete-mode                t)
-(ido-everywhere                t)
-(setq ido-vitrual-buffers      t)
-(setq ido-enable-flex-matching t)
-
-;; Buffer Selection and ibuffer settings
-(require 'bs)
-(require 'ibuffer)
-(defalias 'list-buffers 'ibuffer) ;; отдельный список буферов при нажатии C-x C-b
-(global-set-key (kbd "<f2>") 'bs-show) ;; запуск buffer selection кнопкой F2
 
 (use-package all-the-icons
   :ensure t
@@ -116,182 +43,80 @@
 (use-package all-the-icons-dired
   :hook (dired-mode . (lambda () (all-the-icons-dired-mode t))))
 
-(require 'imenu)
-(setq imenu-auto-rescan      t) ;; автоматически обновлять список функций в буфере
-(setq imenu-use-popup-menu nil) ;; диалоги Imenu только в минибуфере
-(global-set-key (kbd "<f6>") 'imenu) ;; вызов Imenu на F6
+(use-package magit)
+;; for vim-like keybindings in magit
+;(use-package evil-collection
+ ; :ensure t
+  ;:after evil
+  ;:init
+  ;(evil-collection-init))
 
-(show-paren-mode t) ;; включить выделение выражений между {},[],()
+(show-paren-mode t) ;; show parentesis{},[],()
 ;; Delete selection
 (delete-selection-mode t)
 
 (electric-indent-mode -1)
 (electric-pair-mode    1) ;; автозакрытие {},[],() с переводом курсора внутрь скобок
 
-(require 'windmove)
+(global-set-key (kbd "C-<tab>") 'other-window)
+;;change win focus S-<left> etc
+(windmove-default-keybindings)
 
-;;;###autoload
-(defun buf-move-up ()
-  (interactive)
-  (let* ((other-win (windmove-find-other-window 'up))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (null other-win)
-        (error "No window above this one")
-      ;; swap top with this one
-      (set-window-buffer (selected-window) (window-buffer other-win))
-      ;; move this one to top
-      (set-window-buffer other-win buf-this-buf)
-      (select-window other-win))))
+(set-frame-font "Comic Code Demo 20")
+  (hl-line-mode nil)
 
-;;;###autoload
-(defun buf-move-down ()
-  (interactive)
-  (let* ((other-win (windmove-find-other-window 'down))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (or (null other-win) 
-            (string-match "^ \\*Minibuf" (buffer-name (window-buffer other-win))))
-        (error "No window under this one")
-      (set-window-buffer (selected-window) (window-buffer other-win))
-      (set-window-buffer other-win buf-this-buf)
-      (select-window other-win))))
+  ;;theme customization
+  (setq modus-themes-mode-line '(accented borderless padded))
+  (setq modus-themes-region '(bg-only))
+  (setq modus-themes-syntax '(faint))
+  (setq modus-themes-syntax '(green-strings))
 
-;;;###autoload
-(defun buf-move-left ()
-  (interactive)
-  (let* ((other-win (windmove-find-other-window 'left))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (null other-win)
-        (error "No left split")
-      (set-window-buffer (selected-window) (window-buffer other-win))
-      (set-window-buffer other-win buf-this-buf)
-      (select-window other-win))))
+  (setq modus-themes-headings
+        '((1 . (raindow  1.3))
+           (2 . (raindow  1.2))
+           (3 . (raindow  1.1))
+          (t . (semilight 1.1))))
+  (setq modus-themes-scale-headings t)
+  ;(setq modus-themes-org-blocks 'gray-background)
+(load-theme 'modus-vivendi t)
 
-;;;###autoload
-(defun buf-move-right ()
-  (interactive)
-  (let* ((other-win (windmove-find-other-window 'right))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (null other-win)
-        (error "No right split")
-      (set-window-buffer (selected-window) (window-buffer other-win))
-      (set-window-buffer other-win buf-this-buf)
-      (select-window other-win))))
+;; Inhibit startup/splash screen
+(setq inhibit-splash-screen   t)
+(setq ingibit-startup-message t) ;; no hello screen
+;; Disable GUI components
+(tooltip-mode -1)
+(menu-bar-mode -1) 
+(tool-bar-mode  -1)
+(scroll-bar-mode -1) 
+(blink-cursor-mode nil) 
 
-;; Display file size/time in mode-line
+(setq use-dialog-box nil)
+(setq redisplay-dont-pause t)  ;; better buffer rendering 
+(setq ring-bell-function 'ignore) ;; disable bell
+;;(setq visible-bell t) ;; disable bell
+
+(recentf-mode 1)
+
+;; Display time in mode-line
 (setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
 (display-time-mode             t) ;; показывать часы в mode-line
-(global-display-line-numbers-mode nil)
-(global-visual-line-mode t)
+
+;;(global-display-line-numbers-mode nil)
+;;(global-visual-line-mode t)
 (setq display-line-numbers 'relative)
 
-(use-package counsel
-  :after ivy
-  :config (counsel-mode))
+(use-package org-roam)
 
-(use-package ivy
-  :bind
-  ;; ivy-resume resumes the last Ivy-based completion.
-  (("C-c -r" . ivy-resume)
-   ("C-x B" . ivy-switch-buffer-other-window))
-  :custom
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq enable-recursive-minibuffers t)
-  :config
-  (ivy-mode))
-(use-package all-the-icons-ivy-rich
-  :ensure t
-  :init (all-the-icons-ivy-rich-mode 1))
-
-(use-package ivy-rich
-  :after ivy
-  :ensure t
-  :init (ivy-rich-mode 1) ;; this gets us descriptions in M-x.
-  :custom
-  (ivy-virtual-abbreviate 'full
-   ivy-rich-switch-buffer-align-virtual-buffer t
-   ivy-rich-path-style 'abbrev)
-  :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
-
-(use-package toc-org
-    :commands toc-org-enable
-    :init (add-hook 'org-mode-hook 'toc-org-enable))
+(setq org-babel-python-command "python3")
+ (org-babel-do-load-languages
+  'org-babel-load-languages
+  '((python . t)))
 
 (add-hook 'org-mode-hook 'org-indent-mode)
 (use-package org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (require 'org-tempo)
-
-(use-package rainbow-mode
-  :hook org-mode prog-mode)
-
-(defun reload-init-file ()
-  (interactive)
-  (load-file user-init-file)
-  (load-file user-init-file))
-
-(use-package vterm
-:config
-(setq shell-file-name "/bin/fish"
-      vterm-max-scrollback 5000))
-;;press c-q to send unmodifyied key after it 
-;(define-key vterm-mode-map (kbd "C-q") #'vterm-send-next-key)
-
-(use-package vterm-toggle
-  :after vterm
-  :config
-  (setq vterm-toggle-fullscreen-p nil)
-  (setq vterm-toggle-scope 'project)
-  (add-to-list 'display-buffer-alist
-               '((lambda (buffer-or-name _)
-                     (let ((buffer (get-buffer buffer-or-name)))
-                       (with-current-buffer buffer
-                         (or (equal major-mode 'vterm-mode)
-                             (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
-                  (display-buffer-reuse-window display-buffer-at-bottom)
-                  ;;(display-buffer-reuse-window display-buffer-in-direction)
-                  ;;display-buffer-in-direction/direction/dedicated is added in emacs27
-                  ;;(direction . bottom)
-                  ;;(dedicated . t) ;dedicated is supported in emacs27
-                  (reusable-frames . visible)
-                  (window-height . 0.3))))
-
-(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
-(load-theme 'PaleGr13nka t)
-
-(set-face-attribute 'default nil
-  :font "FiraCode Nerd Font"
-  :height 250
-  :weight 'medium)
-(set-face-attribute 'variable-pitch nil
-  :font "FiraCode Nerd Font"
-  :height 250
-  :weight 'medium)
-(set-face-attribute 'fixed-pitch nil
-  :font "FiraCode Nerd Font"
-  :height 250
-  :weight 'medium)
-;; Makes commented text and keywords italics.
-;; This is working in emacsclient but not emacs.
-;; Your font must have an italic face available.
-(set-face-attribute 'font-lock-comment-face nil
-  :slant 'italic)
-(set-face-attribute 'font-lock-keyword-face nil
-  :slant 'italic)
-
-;; This sets the default font on all graphical frames created after restarting Emacs.
-;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
-;; are not right unless I also add this method of setting the default font.
-(add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font"))
-
-;; Uncomment the following line if line spacing needs adjusting.
-(setq-default line-spacing 0.12)
-
-(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
-(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
 (use-package which-key
   :init
