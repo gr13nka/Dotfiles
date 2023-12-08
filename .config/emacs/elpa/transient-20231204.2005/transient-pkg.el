2261444c980ea4dc72d5f6bb69596e28432e4baf
Andrey Grankov <gran-san2003@yandex.ru>
2023-12-09 00:16:40 +0300

    meh

---
+++ .config/emacs/elpa/transient-20231204.2005/transient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "transient" "20231204.2005" "Transient commands"
+  '((emacs "26.1")
+    (compat "29.1.4.4")
+    (seq "2.24"))
+  :commit "2f12f06c689af517ca64acfc51b27422c86b230d" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainers
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .config/sway/change_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+mv config inactive_config1
+mv inactive_config config
+mv inactive_config1 inactive_config
+
